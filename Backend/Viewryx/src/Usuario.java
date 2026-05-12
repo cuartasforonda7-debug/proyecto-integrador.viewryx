dac2ab2b92fa8a9d2eadfa1d401a6c3a0a8e1084
Cristian Cuartas <cuartasforonda.7@gmail.com>
2026-05-11 20:50:12 -0500

    feat: estructura inicial del proyecto integrador

---
+++ Backend/Viewryx/src/Usuario.java
@@ -0,0 +1,16 @@
+public class Usuario {
+    int id;
+    String nombre;
+    String correo;
+    String password;
+    String rol;
+
+    public Usuario(int id, String nombre, String correo, String password, String rol) {
+        this.id = id;
+        this.nombre = nombre;
+        this.correo = correo;
+        this.password = password;
+        this.rol = rol;
+    }
+}
+
---
+++ Backend/Viewryx/src/menuPrincipal.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class menuPrincipal {
+    public static void menuPrincipal(String[] args) {
+        int opcion = 0;
+        Scanner scanner = new Scanner(System.in);
+        do {
+            System.out.println("1. Registrarse");
+            System.out.println("2. Loguearse");
+            System.out.println("3. Salir");
+
+            opcion = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (opcion) {
+                case 1:
+                    registrarUsuario();
+                    break;
+                case 2:
+                    Usuario usuarioLogueado = login();
+                    if (usuarioLogueado != null) {
+                        if (usuarioLogueado.rol.equals("ADMIN")) {
+
+                            menuAdmin();
+                        }
+                    }
+                    break;
+                case 3:
+                    System.out.println("Saliendo...");
+                    break;
+            }
+        } while (opcion != 0);
+
+    }
+}
