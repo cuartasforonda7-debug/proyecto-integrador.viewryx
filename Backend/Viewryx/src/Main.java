ee79b493df606194dab0f797f78de7e1e0ae16db
juanjooyepes-code <juanjoo.yepes@gmail.com>
2026-05-13 14:40:20 -0500

    Add files via upload

---
+++ Backend/Viewryx/src/Main.java
@@ -0,0 +1,21 @@
+package Viewryx;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    static ArrayList<Usuario> listaUsuarios = new
+            ArrayList<>();
+    static ArrayList<Proyecto> listaProyectos = new
+            ArrayList<>();
+    static ArrayList<Personal> listaPersonal = new
+            ArrayList<>();
+    static ArrayList<Actividad> listaActividades = new
+            ArrayList<>();
+
+    static Scanner sc = new Scanner(System.in);
+
+    static int idUsuario = 1;
+    static int idProyect = 1;
+    static int idPersonal = 1;
+    static int idActividad = 1;
